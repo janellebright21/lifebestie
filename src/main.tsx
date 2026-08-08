2d0a7ff1a49a7bf1e2940986a304f232cb98da17
janellebright21 <janellebright21@gmail.com>
2026-08-08 13:05:03 -0400

    Load layered Bestie motion foundations

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './bestie-motion-natural.css';
+import './layered-bestie-motion.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
